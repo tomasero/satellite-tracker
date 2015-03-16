54a585b17dd047ddf358fe99f221f3ef4e10306f
Tomasero <tomasvegagalvez@gmail.com>
2015-03-16 15:07:56 -0700

    cron added

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import requests, json, ephem, subprocess, sys
+import requests, json, ephem, subprocess, sys, crython
 from time import strftime, localtime
 from datetime import datetime, timedelta
 app = Flask(__name__)
@@ -9,28 +9,31 @@ url = 'http://celestrak.com/NORAD/elements/visual.txt'
 my_lat = '51.5033630'
 my_lon = '-0.1276250'
 
+satellite_bodies = {}
+
+@crython.job(expr='@daily')
+def query_satellites():
+	resp = requests.get(url).content
+	resp = map(str.strip, resp.split('\r\n'))
+	TLE_array = {}
+	for i in xrange(0, len(resp)-1, 3):
+		TLE_array[resp[i]] = resp[i:i+3]
+	#bodies
+	for sat, TLE in TLE_array.iteritems():
+		satellite_bodies[sat] = ephem.readtle(TLE[0], TLE[1], TLE[2])
+
+
+
 @app.route('/', methods=['GET'])
 def hello_world():
-
 	#observer
 	obs = ephem.Observer()
 	obs.lat, obs.lon = my_lat, my_lon
 	obs.date = strftime("%Y-%m-%d %H:%M:%S", localtime())
 
 	#TLEs
-	resp = requests.get(url).content
-	resp = map(str.strip, resp.split('\r\n'))
-	TLE_array = {}
-	for i in xrange(0, len(resp)-1, 3):
-		TLE_array[resp[i]] = resp[i:i+3]
-	bodies = {}
-
-	#bodies
-	for sat, TLE in TLE_array.iteritems():
-		bodies[sat] = ephem.readtle(TLE[0], TLE[1], TLE[2])
-
 	visible = []
-	for key, body in bodies.iteritems():
+	for key, body in satellite_bodies.iteritems():
 		info = obs.next_pass(body)
 		rise_time = info[0].datetime()
 		if (rise_time != None) and (rise_time > datetime.now()) and (rise_time < datetime.now() + timedelta(hours=24)):
@@ -46,7 +49,7 @@ def hello_world():
 		print output
 	pipe.stdin.close()
 
-	# print bodies
 
 if __name__ == '__main__':
 	app.run(debug=True)
+	crython.tab.start()
