111051a7a805b375f6ea197f79dcf6cdb42a980c
Tomasero <tomasvegagalvez@gmail.com>
2015-03-23 13:35:42 -0700

    deploying

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests, json, ephem, subprocess, sys, crython
 from time import strftime, localtime
 from datetime import datetime, timedelta
@@ -24,28 +24,39 @@ def query_satellites():
 
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-	#observer
-	query_satellites()
-	obs = ephem.Observer()
-	obs.lat, obs.lon = my_lat, my_lon
-	obs.date = strftime("%Y-%m-%d %H:%M:%S", localtime())
+@app.route('/get_satellites_locations', methods=['POST'])
+def get_satellites_locations():
+	print('-------------------')
+	data = json.loads(request.data)
+	print(data)
+	print(data['latitude'])
+	# print(request.get_json(force=True))
+	# print jsonify(request.data)['latitude']
+	req_latitude = request.data['latitude']
+	req_longitude = request.data['longitude']
+	req_time = request.data['time']
+	# print req_latitude, req_longitude, req_time
+	# return request.args
+	# #observer
+	# query_satellites()
+	# obs = ephem.Observer()
+	# obs.lat, obs.lon = my_lat, my_lon
+	# obs.date = strftime("%Y-%m-%d %H:%M:%S", localtime())
 
-	#TLEs
-	visible = []
-	for key, body in satellite_bodies.iteritems():
-		info = obs.next_pass(body)
-		rise_time = info[0].datetime()
-		if (rise_time != None) and (rise_time > datetime.now()) and (rise_time < datetime.now() + timedelta(hours=24)):
-			visible.append(key)
-	EDB = {}
-	for sat in visible:
-		TLE = TLE_array[sat]
-		EDB[sat] = subprocess.check_output(['./tle2edb.py', TLE[0], TLE[1], TLE[2]]).strip()
-	response = jsonify(EDB)
-	response.status_code = 200
-	return response
+	# #TLEs
+	# visible = []
+	# for key, body in satellite_bodies.iteritems():
+	# 	info = obs.next_pass(body)
+	# 	rise_time = info[0].datetime()
+	# 	if (rise_time != None) and (rise_time > datetime.now()) and (rise_time < datetime.now() + timedelta(hours=24)):
+	# 		visible.append(key)
+	# EDB = {}
+	# for sat in visible:
+	# 	TLE = TLE_array[sat]
+	# 	EDB[sat] = subprocess.check_output(['./tle2edb.py', TLE[0], TLE[1], TLE[2]]).strip()
+	# response = jsonify(EDB)
+	# response.status_code = 200
+	# return response
 
 if __name__ == '__main__':
 	crython.tab.start()
