a16680117c627d83446c6eb7831dc263cafad430
Tomasero <tomasvegagalvez@gmail.com>
2015-03-28 19:24:00 -0700

    works

--- README.md
+++ README.md
@@ -18,3 +18,7 @@
 - Nick Firmani
 - Rundong Tian
 - Tomas Vega
+
+###Notes
+Test with the following command
+	curl --data "latitude=37.877652&longitude=-122.262247&time=15/03/28,06:20:20%2B12" http://localhost:5000/get_satellites_locations	
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 import requests, json, ephem, subprocess, sys, crython
 from time import strftime, localtime
 from datetime import datetime, timedelta
+import re
 
 app = Flask(__name__)
 
@@ -30,27 +31,51 @@ def get_satellites_locations():
 	latitude = params['latitude']
 	longitude = params['longitude']
 	time = params['time']
-	print latitude, longitude, time
-	# #observer
-	# query_satellites()
-	# obs = ephem.Observer()
-	# obs.lat, obs.lon = my_lat, my_lon
-	# obs.date = strftime("%Y-%m-%d %H:%M:%S", localtime())
+	parsed_time = parse_time(time)
+	# print latitude, longitude, time
+	#observer
+	query_satellites()
+	obs = ephem.Observer()
+	obs.lat, obs.lon = latitude, longitude
+	obs.date = parsed_time
+	
+	#TLEs
+	visible = []
+	for key, body in satellite_bodies.iteritems():
+		info = obs.next_pass(body)
+		rise_time = info[0].datetime()
+		if (rise_time != None) and (rise_time > datetime.now()) and (rise_time < datetime.now() + timedelta(hours=24)):
+			visible.append(key)
+	EDB = {}
+	for sat in visible:
+		TLE = TLE_array[sat]
+		EDB[sat] = subprocess.check_output(['./tle2edb.py', TLE[0], TLE[1], TLE[2]]).strip()
+	response = jsonify(EDB)
+	response.status_code = 200
+	return response
 
-	# #TLEs
-	# visible = []
-	# for key, body in satellite_bodies.iteritems():
-	# 	info = obs.next_pass(body)
-	# 	rise_time = info[0].datetime()
-	# 	if (rise_time != None) and (rise_time > datetime.now()) and (rise_time < datetime.now() + timedelta(hours=24)):
-	# 		visible.append(key)
-	# EDB = {}
-	# for sat in visible:
-	# 	TLE = TLE_array[sat]
-	# 	EDB[sat] = subprocess.check_output(['./tle2edb.py', TLE[0], TLE[1], TLE[2]]).strip()
-	# response = jsonify(EDB)
-	# response.status_code = 200
-	# return response
+def parse_time(time):
+	year = int(time[:2])
+	if year > datetime.now().year%1000:
+		year += 1900
+	else:
+		year += 2000
+	month = int(time[3:5])
+	day = int(time[6:8])
+	hour = int(time[9:11])
+	minute = int(time[12:14])
+	second = int(time[15:17])
+	operand = time[17]
+	zone = int(time[18:21])
+	if operand == '+':
+		delta = timedelta(hours=zone)
+	else:
+		delta = timedelta(hours=-zone)
+	dt = datetime(year, month, day, hour, minute, second)
+	dt = dt + delta
+	return dt
+
+	# print year, month, day, hour, minute, second, tzinfo
 
 if __name__ == '__main__':
 	crython.tab.start()
